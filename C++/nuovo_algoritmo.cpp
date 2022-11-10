ae03c01c95aac1be84189fad1f42e13ac049e64f
Francesco Michieletto <103146767+michifx512@users.noreply.github.com>
2022-11-10 20:07:08 +0100

    updated nuovo algoritmo

--- C++/nuovo_algoritmo.cpp
+++ C++/nuovo_algoritmo.cpp
@@ -6,83 +6,109 @@ using namespace std;
 int main() {
 	srand((int)time(0));
 
+    int flag=0, pneri=0, pbia=0;
     int tentativi=9, lung=4;
-    int vcasuale[4], v_in[4], comuniunivoci[4]={0};
-
+    int cas[4], pin[4], vcu[4]={0};
+    int du=0, de=0; //dimensioni vettore univoci, du=dim univoci, de= dim extra
     bool boolneri[4] = {false};
-
-    int flag=0;
 
     for(int z=0; z<4; z++){
         cout << "\nInserisci il numero generato [" << z << "]: ";
-        cin >> vcasuale[z];
+        cin >> cas[z];
     }
 
     for(int i=0; i<tentativi; i++){
 
-
-
-        cout << "\nTuo tentativo n." << i+1 << ": ";
+        cout << "\n\n\nTuo tentativo n." << i+1 << ": ";
 
         for(int j=0; j<4; j++){
             cout << "\nInserisci il numero  [" << j << "]: ";
-            cin >> v_in[j];
+            cin >> pin[j];
             boolneri[j]=false;
         }
+        cout << "\n\nRISULTATO:  ";
 
+        pneri=0;pbia=0;
         for(int j=0; j<lung; j++){ // controllo neri
-            if(vcasuale[j]==v_in[j]){ // c == pin
+            if(cas[j]==pin[j]){ // c == pin
                 boolneri[j] = true;
-                cout<<"n";
+                pneri++;
+                cout<<"N ";
             }
         }
-/*
-        int dvu=0; //dimensione vettore univoci
+
+        du=0;
         for(int j=0; j<lung; j++){
             for(int k=0; k<lung; k++){
-                if (v_in[j]==vcasuale[k] && !boolneri[j] && !boolneri[k]) {
+                if (pin[j]==cas[k] && !boolneri[j] && !boolneri[k]) {
                     flag = 0;
-                    for (int x = 0; x < dvu; x++) {
-                        if (comuniunivoci[x] == v_in[j]) {
+                    for (int x = 0; x < du; x++) {
+                        if (vcu[x] == pin[j]) {
                             flag++;
                         }
                     }
                     if (flag == 0) {
-                        comuniunivoci[dvu] = v_in[j];
-                        dvu++;
+                        vcu[du] = pin[j];
+                        du++;
                     }
                 }
             }
         }
-        for(int j=0; j<dvu; j++){
-            cout << "\n" << comuniunivoci[j];
-            cout <<"b";
-        }*/
+        de=du;
+        for(int j=0; j<du; j++){
+            int cv1=0, cv2=0;
+            for(int k=0; k<lung; k++){
+                if(vcu[j]==pin[k] && !boolneri[k]){
+                    cv1++;
+                }
+                if(vcu[j]==cas[k] && !boolneri[k]){
+                    cv2++;
+                }
+            }
+            if (cv1==2 && cv2 == 2){
+                vcu[de] = vcu[j];
+                de++;
+            }
+        }
 
+        // da ora si userà de al posto di du 
+        for(int j=0; j<de; j++){
+            cout <<"B ";
+        }
+        cout << "\nVettore Univoci: ";
+        for(int j=0; j<du; j++){
+            cout << vcu[j] << "\t";
+		}
+        cout << "\nVettore Univoci Extra: ";
+        for(int j=0; j<de; j++){
+            cout << vcu[j] << "\t";
+		}
 
-        int dvu=1; //dimensione vettore univoci
+/*
+        int du=1; //dimensione vettore univoci
         for(int j=0; j<lung; j++){
             for(int k=0; k<lung; k++){
-                if (v_in[j]==vcasuale[k] && !boolneri[j] && !boolneri[k]) {
+                if (pin[j]==cas[k] && !boolneri[j] && !boolneri[k]) {
                     flag = 0;
-                    for (int x = 1; x <=dvu; x++) {
-                        if (comuniunivoci[x-1] == v_in[j]) {
+                    for (int x = 1; x <=du; x++) {
+                        if (vcu[x-1] == pin[j]) {
                             flag++;
                         }
                     }
                     if (flag == 0) {
-                        comuniunivoci[dvu-1] = v_in[j];
-                        dvu++;
+                        vcu[du-1] = pin[j];
+                        du++;
                     }
                 }
             }
         }
 
         
-        for(int j=1; j<dvu; j++){
-            cout << "\n" << comuniunivoci[j-1];
+        for(int j=1; j<du; j++){
+            cout << "\n" << vcu[j-1];
             cout <<"b";
         }
+*/
     }
 	cout << endl;
 	system("pause");
